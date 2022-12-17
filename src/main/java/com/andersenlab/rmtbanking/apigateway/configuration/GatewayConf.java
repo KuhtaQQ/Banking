33d2bad9460c90478a69a98062caa0be8f7c0e9a
arctg07 <arctg07@gmail.com>
2022-12-18 00:29:06 +0400

    fix

--- src/main/java/com/andersenlab/rmtbanking/apigateway/configuration/GatewayConf.java
+++ src/main/java/com/andersenlab/rmtbanking/apigateway/configuration/GatewayConf.java
@@ -8,23 +8,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConf {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder
-                .routes()
-                .route(predicate -> predicate
-                         .path("/auth/deposit-products/**", "/auth/cards-products/")
-                         .uri("http://10.10.14.11:9003"))
-                .route(predicate -> predicate
-                        .path("/exchange-rates")
-                        .uri("http://10.10.14.53:9006"))
-                .route(predicate -> predicate
-                        .path("/currency")
-                        .uri("http://10.10.14.53:9006"))
-                .route(predicate -> predicate
-                        .path("/credit-products")
-                        .uri("http://10.10.14.11:9002"))
-                .build();
-
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder
+//                .routes()
+//                .route(predicate -> predicate
+//                         .path("/auth/deposit-products/**", "/auth/cards-products/")
+//                         .uri("http://10.10.14.11:9003"))
+//                .route(predicate -> predicate
+//                        .path("/exchange-rates")
+//                        .uri("http://10.10.14.53:9006"))
+//                .route(predicate -> predicate
+//                        .path("/currency")
+//                        .uri("http://10.10.14.53:9006"))
+//                .route(predicate -> predicate
+//                        .path("/credit-products")
+//                        .uri("http://10.10.14.11:9002"))
+//                .build();
+//    }
 }
