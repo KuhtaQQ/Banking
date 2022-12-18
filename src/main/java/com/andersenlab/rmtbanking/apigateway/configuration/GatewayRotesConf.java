e47d167ff58652feb4ec6bbf00e4014bbfbc9e90
arctg07 <arctg07@gmail.com>
2022-12-18 23:11:06 +0400

    fix

--- src/main/java/com/andersenlab/rmtbanking/apigateway/configuration/GatewayRotesConf.java
+++ src/main/java/com/andersenlab/rmtbanking/apigateway/configuration/GatewayRotesConf.java
@@ -12,10 +12,19 @@ public class GatewayRotesConf {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                .route("1",
-                        predicate -> predicate
-                                .path("/cards-products")
-                                .uri("http://10.10.14.53:9003/auth/cards-products"))
+                .route(predicate -> predicate
+                         .path("/auth/deposit-products/**", "/auth/cards-products/")
+                         .uri("http://10.10.14.11:9003"))
+                .route(predicate -> predicate
+                        .path("/exchange-rates")
+                        .uri("http://10.10.14.53:9006"))
+                .route(predicate -> predicate
+                        .path("/currency")
+                        .uri("http://10.10.14.53:9006"))
+                .route(predicate -> predicate
+                        .path("/credit-products")
+                        .uri("http://10.10.14.11:9002"))
+
 //                .route("2",
 //                        predicate -> predicate
 //                                .path("deposit/cards-products",
